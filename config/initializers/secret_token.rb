8d5304a371c117cc0e116ac14ba08c0b61d2a0ed
Maciek Wątroba <maciek.bb@gmail.com>
2013-10-17 18:28:59 +0200

    authentication & bootstrap project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ExchangeSubject::Application.config.secret_key_base = 'c990328e6b9a6bfb8c701ae717af4c74620374066d767d7ce5c2ed9087db435f63281db2037cdec15156aecaca3e6cd5ef18da9af9fd95d92ba19d992f0af77a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ExchangeSubject::Application.config.session_store :cookie_store, key: '_ExchangeSubject_session'
